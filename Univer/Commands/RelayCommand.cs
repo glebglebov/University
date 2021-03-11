9cbeb9f5fe5fa805876c3d68b7f446bccf8caa3c
Gleb Shalimov <glebshalimovru@gmail.com>
2021-03-11 17:51:59 +0300

    RelayCommand pattern

--- Univer/Commands/RelayCommand.cs
+++ Univer/Commands/RelayCommand.cs
@@ -1,10 +1,35 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using System.Windows.Input;
 
 namespace Univer.Commands
 {
-    class RelayCommand
+    class RelayCommand : ICommand
     {
+        private Action<object> _Execute;
+        private Func<object, bool> _CanExecute;
+
+        public event EventHandler CanExecuteChanged
+        {
+            add
+            {
+                CommandManager.RequerySuggested += value;
+            }
+            remove
+            {
+                CommandManager.RequerySuggested -= value;
+            }
+        }
+
+        public RelayCommand(Action<object> Execute, Func<object, bool> CanExecute)
+        {
+            _Execute = Execute;
+            _CanExecute = CanExecute;
+        }
+
+        public void Execute(object parameter) => _Execute(parameter);
+
+        public bool CanExecute(object parameter) => _CanExecute?.Invoke(parameter) ?? true;
     }
 }
